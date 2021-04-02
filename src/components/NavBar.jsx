09f6336f9f1888d771620997dcb22b7f58abae3d
Vamshi <pvmsikrsna@gmail.com>
2021-04-02 15:59:44 +0530

    Added responsive nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Form, FormControl, Nav, Navbar} from "react-bootstrap";
+
+const NavBar = ({search, onSearchChange}) => {
+  return <>
+    <Navbar bg="light" expand="md">
+      <Navbar.Brand href="#home">Issues Server</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/issues">Issues</Nav.Link>
+        </Nav>
+        <Form>
+          <FormControl type="text" value={search} onChange={onSearchChange}
+                       placeholder="Search" className="mr-sm-2" />
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  </>
+}
+
+export default NavBar;
